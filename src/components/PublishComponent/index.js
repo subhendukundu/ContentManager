2169ec2b2251327ebc0cb2f36921aafb890de656
Subh <skund2@sapient.com>
2018-11-16 20:13:55 +0530

    feat: adding component manager init

--- src/components/PublishComponent/index.js
+++ src/components/PublishComponent/index.js
@@ -1,8 +1,12 @@
 import React, {Component} from 'react';
-import { Row, Col, Collection, CollectionItem, Card, Button, Icon, Modal, Input } from 'react-materialize';
+import * as ReactMaterialize from 'react-materialize';
 import SortableTree, { addNodeUnderParent, removeNodeAtPath, changeNodeAtPath } from 'react-sortable-tree';
 import FileExplorerTheme from 'react-sortable-tree-theme-full-node-drag';
+import Preview from 'react-component-preview';
 import './style.scss';
+
+
+const { Row, Col, Collection, CollectionItem, Card, Button, Icon, Modal, Input } = ReactMaterialize;
 
 const styles = {
     scroll: {
@@ -29,18 +33,24 @@ export default class PublishComponent extends Component {
         this.state = {
 			treeData: [
 				{
-					title: 'Add a page name',
+					title: 'Page Name',
+					subtitle: "",
 				  	id: 'pageName',
 				  	expanded: true,
 				  	canDrag: false
 				}
 			],
+			node: {},
+			path: [],
 			dataProps: {},
-			webPageName: 'Add a page name'
+			submitData: {},
+			webPageName: 'Page Name',
+			dataForFirebase: {}
         };
 	}
 
 	addThisComponentToTree = (e, data, key, getNodeKey) => {
+		console.log(data)
 		this.setState(state => ({
 			treeData: addNodeUnderParent({
 			  treeData: state.treeData,
@@ -49,45 +59,115 @@ export default class PublishComponent extends Component {
 			  getNodeKey,
 			  newNode: {
 				title: key,
+				data: data
 			  },
 			}).treeData,
 		}));
 		window.Materialize.toast(`${key} added!`, 4000);
 	}
 
-	sendComponentDetailsToModal = (node) => {
-		const { id, title } = node;
-		console.log(node);
+	sendComponentDetailsToModal = (node, path) => {
+		const { id, title, subtitle } = node;
 		const { componentlist } = this.props;
-		const dataProps = (id === 'pageName') ? {title} : componentlist[title].dataProps;
-		this.setState({dataProps}, () => {
+		const dataProps = (id === 'pageName') ? {webPageName: subtitle} : componentlist[title].dataProps;
+		this.setState({dataProps, node, path}, () => {
 			$('#publishChoice').modal().modal('open');
 		});
 	}
 
-	changeInputDetails = (e, key) => {
-		console.log(key);
+	changeInputDetails = (key) => (e) => {
+		const { node, path, submitData } = this.state;
+		const { title } = node;
 		const {value} = e.target;
-		if(webPageName === key) {
+		const { webPageName } = this.state;
+		if(title === 'Page Name') {
 			this.setState({
 				webPageName: value
 			});
-		} else {
-			
 		}
+		submitData[title] = Object.assign({}, submitData[title], { [key] : value});
+
+		this.setState({ submitData });
 	}
 
-	changeComponentDetails = (node, path, getNodeKey) => {
-		const { treeData, webPageName } = this.state;
+	submitNewPage = () => {
+		const { state, props } = this;
+		const { dataForFirebase, submitData } = state;
+		const { webPageName } = submitData['Page Name'];
+		window.Materialize.toast(`${webPageName} page added!`, 4000);
+		$('#showComponents').modal('close');
+		const dbCon = props.db.database().ref(`/pages/${webPageName}`);
+		dbCon.set({
+			dataForFirebase
+		});
+	}
+
+	getallCompoennt = (children) => {
+		const allCompoennt = [];
+		for (let key of children) {
+			const { data } = key;
+			if (allCompoennt[key['title']]) {
+				const firstDataToPush = {};
+				Object.keys(data).forEach(item => {
+					if(typeof(data[item]) === 'string') {
+						firstDataToPush[item] = data[item];
+					} else if (Array.isArray(data[item])) {
+						firstDataToPush[item] = data[item][0];
+					}
+				});
+				allCompoennt[key['title']].push(firstDataToPush);
+			} else {
+				const firstDataToPush = {};
+				Object.keys(data).forEach(item => {
+					if(typeof(data[item]) === 'string') {
+						firstDataToPush[item] = data[item];
+					} else if (Array.isArray(data[item])) {
+						firstDataToPush[item] = data[item][0];
+					}
+				});
+				allCompoennt[key['title']] = [firstDataToPush]
+			}
+		}
+		return allCompoennt;
+	}
+
+	getAllDetails = () => {
+		const { node, path, treeData, webPageName, submitData } = this.state;
+		console.log(treeData);
+		const allCompoennt = [];
+		const dataForFirebase = Object.assign({}, {
+			webPageName: treeData[0].subtitle,
+			componentNames:  treeData[0].children ? this.getallCompoennt(treeData[0].children) : []
+		});
+		console.log(dataForFirebase);
+		this.setState({dataForFirebase}, () => {
+			$('#showComponents').modal().modal('open');
+		});
+	}
+
+	changeComponentDetails = (getNodeKey) => {
+		const { node, path, treeData, webPageName, submitData } = this.state;
+		console.log(node, path, treeData, webPageName, submitData)
 		const { title } = node;
-		this.setState(state => ({
-			treeData: changeNodeAtPath({
-			  treeData: state.treeData,
-			  path,
-			  getNodeKey,
-			  newNode: { ...node, title: webPageName },
-			}),
-		}));
+		if (title === 'Page Name') {
+			this.setState(state => ({
+				treeData: changeNodeAtPath({
+				  treeData: state.treeData,
+				  path,
+				  getNodeKey,
+				  newNode: { ...node, subtitle: webPageName },
+				}),
+			}));
+		} else {
+			this.setState(state => ({
+				treeData: changeNodeAtPath({
+				  treeData: state.treeData,
+				  path,
+				  getNodeKey,
+				  newNode: { ...node, data: Object.assign({}, node.data, submitData[title])},
+				}),
+			}));
+		}
 		$('#publishChoice').modal('close');
 	}
 
@@ -96,24 +176,18 @@ export default class PublishComponent extends Component {
 		return optionsForSelect;
 	}
 
+	/* getAllChildren = (children) => Array.isArray(children) ? children.map( item => {
+		const componentName = Object.keys(item)[0];
+		const ComponentToRender = ReactMaterialize[componentName];
+		console.log(ComponentToRender);
+
+	}) : children; */
+
 	render () {
 		const { props, state } = this;
 		const { componentlist } = props;
-		const { dataProps, treeData } = state;
-
-		console.log(treeData);
-
-		const modalDetailsArr = Object.keys(dataProps).map((key, index) => {
-			return (
-				<Row>
-					{(typeof(dataProps[key]) === 'string') && <Input s={12} label={key} onChange={e => this.changeInputDetails(e, dataProps[key])} validate defaultValue={dataProps[key]} />}
-					{(Array.isArray(dataProps[key])) && <Input s={12} type='select' label={key} defaultValue={dataProps[key][0]}>
-						{this.modalDetailsWithSelect(dataProps[key])}
-						</Input>
-					}
-				</Row>
-			);
-		});
+		const { dataProps, treeData, submitData, dataForFirebase } = state;
+		const { webPageName, componentNames } = dataForFirebase;
 
 		const cardDetailsArr = Object.keys(componentlist).map((key, index) => {
 			const componentName = key;
@@ -125,6 +199,30 @@ export default class PublishComponent extends Component {
 						actions={[<Button onClick={e => this.addThisComponentToTree(e, data, key, getNodeKey)} waves="light">Add This component <Icon right>add</Icon></Button>]}>
 					</Card>
 				</CollectionItem>
+			);
+		});
+
+		const componentlistNames = componentNames ?  Object.keys(componentNames).map((key, index) => {
+			const ComponentToRender = ReactMaterialize[key];
+			return componentNames[key].map(all => {
+				return (
+					<div>
+						<ComponentToRender {...all}>{all.children}</ComponentToRender>
+					</div>
+				)
+			})
+		}) : '';
+
+		const modalDetailsArr = Object.keys(dataProps).map((key, index) => {
+			const { title } = state.node;
+			return (
+				<Row>
+					{(typeof(dataProps[key]) === 'string') && <Input s={12} label={key} onChange={this.changeInputDetails(key)} validate value={submitData[title] ? submitData[title].key : ''} />}
+					{(Array.isArray(dataProps[key])) && <Input s={12} type='select' onChange={this.changeInputDetails(key)} label={key} defaultValue='{dataProps[key][0]}'>
+						{this.modalDetailsWithSelect(dataProps[key])}
+						</Input>
+					}
+				</Row>
 			);
 		});
 
@@ -158,25 +256,11 @@ export default class PublishComponent extends Component {
 								<Button
 									waves='light'
 									style={styles.button}
-									onClick={() => {this.sendComponentDetailsToModal(node)}}
+									onClick={() => {this.sendComponentDetailsToModal(node, path)}}
 								>
 								Edit This
 								<Icon right>mode edit</Icon>
 								</Button>,
-								<Modal
-									header="Publish Data"
-									id="publishChoice"
-									fixedFooter
-									key={`componentName-${0}`}
-									actions={
-										<div>
-										<Button modal="close" waves="light">dismiss</Button>
-										<Button waves="light" onClick={e => this.changeComponentDetails(node, path, getNodeKey)}>Submit</Button>
-										</div>
-									}
-								>
-								{modalDetailsArr}
-								</Modal>,
 								<Button
 									waves='light'
 									onClick={() => {
@@ -197,6 +281,35 @@ export default class PublishComponent extends Component {
 						})}
 					/>
 				</Col>
+				<Modal
+					header="Publish Data"
+					id="publishChoice"
+					fixedFooter
+					key={`componentName-${0}`}
+					actions={
+						<div>
+						<Button modal="close" waves="light">dismiss</Button>
+						<Button waves="light" onClick={e => this.changeComponentDetails(getNodeKey)}>Submit</Button>
+						</div>
+					}
+				>
+				{modalDetailsArr}
+				</Modal>
+				<Modal
+					id='showComponents'
+					header='Page'
+					actions={
+						<div>
+						<Button modal="close" waves="light">Dismiss</Button>
+						<Button waves="light" onClick={this.submitNewPage}>Submit</Button>
+						</div>
+					}
+				>
+				<div>
+					{componentlistNames}
+				</div>
+				</Modal>
+				<Button waves='light' onClick={this.getAllDetails} large style={{bottom: '45px', right: '0', position: 'fixed'}}>PreView<Icon left>assessment</Icon></Button>
 			</Row>
 		);
 	}
